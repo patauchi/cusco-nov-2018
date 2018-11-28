2e3dde36b02c078c4512b25fea5a2186d6da51d2
P. Joser Atauchi <patauchi@gmail.com>
2018-11-28 05:47:57 -0500

    Update Instalar-paquetes-gis.R

--- Instalar-paquetes-gis.R
+++ Instalar-paquetes-gis.R
@@ -12,9 +12,5 @@ list.of.packages <- c("sdStaf", "dismo", "spThin", "spocc", "devtools",
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-# Instalar librera modelos de nicho
-devtools::install_github('marlonecobos/kuenm')
-
-
 #
 #
